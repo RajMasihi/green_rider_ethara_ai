5cdad287276442ad62fc4519eed5a0b5fb8ff455
Raj Masihi <rajmasihi6397@gmail.com>
2026-05-20 00:41:19 +0530

    fix errors

--- taskmanagement_team/teamtaskmanagement/settings.py
+++ taskmanagement_team/teamtaskmanagement/settings.py
@@ -74,11 +74,11 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
-# CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS =os.getenv(
-      "CORS_ALLOWED_ORIGINS",
-      ""
-    ).split(",")
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS =os.getenv(
+#       "CORS_ALLOWED_ORIGINS",
+#       ""
+#     ).split(",")
 
 ROOT_URLCONF = 'teamtaskmanagement.urls'
 
