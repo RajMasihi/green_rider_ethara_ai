2c7c7bf6c9ac4fd158d210c6b52bd49f32e823cc
Raj Masihi <rajmasihi6397@gmail.com>
2026-05-29 12:02:25 +0530

    resolve path 1.1

--- taskfront/src/routes/AppRoutes.jsx
+++ taskfront/src/routes/AppRoutes.jsx
@@ -12,7 +12,7 @@ function AppRoutes() {
       <Routes>
         {/* Login */}
         <Route path="" element={<SignUp />} />
-        <Route path="login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
 
         {/* Admin */}
         <Route
